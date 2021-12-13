722869524e408044c3fc8da7cc5a83be44774a20
Your Name <you@example.com>
2021-12-13 15:11:43 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+from azure.cosmos.partition_key import PartitionKey
+
+import config
+
+HOST = config.settings['host']
+MASTER_KEY = config.settings['master_key']
+DATABASE_ID = config.settings['database_id']
+CONTAINER_ID = config.settings['container_id']
+
+client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
+
+try:
+    db = client.get_database_client(DATABASE_ID)
+    container = db.get_container_client(id)
+    data = container.query_items(query='SELECT * FROM container', enable_cross_partition_query=True,)
+except exceptions.CosmosHttpResponseError as e:
+    print('\nrun_sample has caught an error. {0}'.format(e.message))
+
+finally:
+    print("\nrun_sample done")
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('testweb.html', data=data)
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/testfun.js
@@ -0,0 +1,18 @@
+function testfun(){
+const date = new Date(Date.now());
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+axios.post('https://prod-74.eastus.logic.azure.com:443/workflows/c10c14e05ec44366a5ae4e9d89270845/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=T14cQAso1_Od0w7xWSQXF69NK5mNCC1UFjNGP6-Xtb0', {
+    id: self.crypto.randomUUID(),
+    month: months[date.getMonth()],
+    day: date.getDate(),
+    year: date.getFullYear(),
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds()
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });}
