2667efa597585ad4796ae88f59ab72b3ba46663a
BrianDeWaltACC <73319239+BrianDeWaltACC@users.noreply.github.com>
2021-12-14 13:58:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
 
 try:
     db = client.get_database_client(DATABASE_ID)
-    container = db.get_container_client(id)
+    container = db.get_container_client(CONTAINER_ID)
     data = container.query_items(query='SELECT * FROM container', enable_cross_partition_query=True,)
 except exceptions.CosmosHttpResponseError as e:
     print('\nrun_sample has caught an error. {0}'.format(e.message))
