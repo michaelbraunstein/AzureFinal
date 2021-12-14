60585797426e8b8ccb1fe19821d1b6769ed1e982
BrianDeWaltACC <73319239+BrianDeWaltACC@users.noreply.github.com>
2021-12-14 15:00:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,20 @@ CONTAINER_ID = config.settings['container_id']
 
 client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
 
-try:
-    db = client.get_database_client(DATABASE_ID)
-    container = db.get_container_client(CONTAINER_ID)
-    data = container.query_items(query='SELECT * FROM container', enable_cross_partition_query=True,)
-except exceptions.CosmosHttpResponseError as e:
-    print('\nrun_sample has caught an error. {0}'.format(e.message))
 
-finally:
-    print("\nrun_sample done")
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    try:
+        db = client.get_database_client(DATABASE_ID)
+        container = db.get_container_client(CONTAINER_ID)
+        data = container.query_items(query='SELECT * FROM container', enable_cross_partition_query=True,)
+    except exceptions.CosmosHttpResponseError as e:
+        print('\nrun_sample has caught an error. {0}'.format(e.message))
+
+    finally:
+        print("\nrun_sample done")
     return render_template('testweb.html', data=data)
 
 if __name__ == '__main__':
