64d63aad04a6a0e7daa6e0db77e50265a745ec71
Michael B <1012481+michaelbraunstein@users.noreply.github.com>
2022-01-27 20:08:32 -0500

    updated url

--- static/testfun.js
+++ static/testfun.js
@@ -1,7 +1,7 @@
 function testfun(){
 const date = new Date(Date.now());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let x = axios.post('REPLACE WITH URL FOR LOGIC APP', {
+let x = axios.post(' https://prod-06.eastus.logic.azure.com:443/workflows/1adeb58c93574ff0ac7b674aec85010d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1vYKW12IORdiJlExQMZ5pQ3IfP6oqbYAwfoSwG5tzm8', {
     id: self.crypto.randomUUID(),
     month: months[date.getMonth()],
     day: date.getDate(),
